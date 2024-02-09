19ee692e86612f12fd853194d17df59b7b9cb1be
thomas-bailly <thomas.bailly1698@gmail.com>
2024-02-09 12:41:06 +0100

    feat: add function to read configuration file in yaml format

--- run_asmc.py
+++ run_asmc.py
@@ -2,11 +2,93 @@ import sys
 import argparse
 import datetime
 import logging
+import subprocess
+import yaml
 from pathlib import Path
 
 ###############
 ## Functions ##
 ###############
+
+def read_yaml():
+    """Read the yaml file
+    
+    Load the content of the yaml file and check the validity of paths.
+    The configuration should be place in a directory named data in the same
+    location as run_asmc.py, e.g :
+
+    .
+    ├── data
+    │   ├── AA_distances.tsv
+    │   └── config.yml
+    ├── run_asmc.py
+    └── src
+        ├── build_ali.py
+        └── modeling.py
+
+
+    Args:
+        args (argparse.Namespace): the object containing all arguments
+
+    Returns:
+        yml (dict): a dictionary corresponding to the contents of the yaml file
+    """
+    
+    main_path = Path(__file__).absolute()
+    parent_path = main_path.parent
+    data_path = Path.joinpath(parent_path, "data")
+    
+    config_path = Path.joinpath(data_path, "config_asmc.yml")
+
+    if not Path.is_file(config_path):
+        logging.error(f"not found the configuration file: {config_path}")
+        sys.exit(1)
+    
+    with open(config_path, "r") as f:
+        yml = yaml.safe_load(f)
+        
+    for key in yml:
+        
+        if key == "distances":
+            if not Path(yml[key]).exists():
+                logging.error(f"{yml[key]} doesn't exist")
+        
+        elif key == "usalign":
+            command = f"{yml[key]} -h"
+            try:
+                ret = subprocess.run(command.split(), capture_output=True)
+            except Exception as error:
+                logging.error(f"An error has occured with USalign:\n{error} ")
+                sys.exit(1)
+            
+            if ret.returncode != 0:
+                logging.error(f"{ret.stderr.decode('utf-8')}")
+                sys.exit(1)
+        
+        elif key == "weblogo":
+            command = f"{yml[key]} --version"
+            try:
+                ret = subprocess.run(command.split(), capture_output=True)
+            except Exception as error:
+                logging.error(f"An error has occured with weblogo:\n{error}")
+                sys.exit(1)
+                
+            if ret.returncode != 0:
+                logging.error(f"{ret.stderr.decode('utf-8')}")
+                sys.exit(1)
+                
+        elif key == "java":
+            command = f"{yml[key]} --version"
+            try:
+                ret = subprocess.run(command.split(), capture_output=True)
+            except Exception as error:
+                logging.error(f"An error has occured with java:\n{error}")
+                
+            if ret.returncode != 0:
+                logging.error(f"{ret.stderr.decode('utf-8')}")
+                sys.exit(1)
+        
+    return yml
 
 ##########
 ## MAIN ##
@@ -35,6 +117,9 @@ if __name__ == "__main__":
         logging.basicConfig(level=logging.INFO,
                     format="%(levelname)s - %(asctime)s - %(message)s",
                     filename=args.log, )
+    
+    # Read the Config file
+    yml = read_yaml()
     
     # Make output directory if doesn't exist 
     outdir = Path(args.outdir).absolute()
