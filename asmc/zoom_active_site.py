27475ca4c116ae9e9b2734053c0a177b556189ed
thomas-bailly <thomas.bailly1698@gmail.com>
2024-04-24 09:43:11 +0200

    style: update of commentaries

--- asmc/zoom_active_site.py
+++ asmc/zoom_active_site.py
@@ -54,7 +54,7 @@ def active_site(active_site_file):
     
     # Get the object list
     object_list = cmd.get_object_list('(all)')
-    # Get the name of teh reference structure object
+    # Get the name of the reference structure object
     object_ref_name = [elem for elem in object_list if "-" in elem][0]
     ref_name = object_ref_name.split('-')[-1]
     target_name = object_ref_name.split("-")[0]
@@ -62,7 +62,7 @@ def active_site(active_site_file):
     # Read the pocket/active_site_file
     chain, pos = get_active_site_pos(ref_name, active_site_path)
     
-    # Create reference active site ofbject
+    # Create reference active site object
     string_selection = f"model {object_ref_name} and chain {chain} and resi {'+'.join(pos)}"
     ref_active_site_name = f"{ref_name}_active_site"
     cmd.create(ref_active_site_name, string_selection)
@@ -78,8 +78,8 @@ def active_site(active_site_file):
     
     # For each residue in the reference active site
     for i, (resi, resn) in enumerate(space["ref"]):
+
         # Search for the target residue aligned with it
-        #string_selection = f"{target_name} and br. all and name CA near_to 1.0 of {ref_active_site_name} and resi {resi} and name CA"
         string_selection = f"{target_name} and br. all near_to 1.0 of {ref_active_site_name} and resi {resi}"
         inner_space = {"res":[]}
         if i == 0:
